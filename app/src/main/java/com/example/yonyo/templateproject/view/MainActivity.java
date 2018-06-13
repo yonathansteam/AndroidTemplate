0d833e67c1e1c703d873c63a4c76a2189d0dc31b
Yonathan <yonathan.steam@gmail.com>
2018-06-13 20:53:16 +0700

    Add kotlin plugin

--- app/src/main/java/com/example/yonyo/templateproject/view/MainActivity.java
+++ app/src/main/java/com/example/yonyo/templateproject/view/MainActivity.java
@@ -19,40 +19,4 @@ public class MainActivity extends AppCompatActivity {
         MainFragment mainFragment = MainFragment.newInstance();
         getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, mainFragment).commitAllowingStateLoss();
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.v(tag, "on start");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.v(tag, "on destroy");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.v(tag, "on restart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.v(tag, "on pause");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.v(tag, "on resume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.v(tag, "on stop");
-    }
 }
